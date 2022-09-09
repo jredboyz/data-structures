d0906cd19cb9480887899792ac9772ead33b74c2
Joseph <jredboyz@gmail.com>
2022-09-08 20:43:22 -0700

    pass test for functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var numkeys = 0;
+  var nextOut = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[numkeys++] = value;
+    // Object.assign(someInstance, storage);
   };
 
   someInstance.dequeue = function() {
+
+    var saved = storage[nextOut];
+    console.log(saved);
+    delete storage[nextOut];
+    if (someInstance.size()) {
+      nextOut++;
+    }
+    return saved;
   };
 
   someInstance.size = function() {
+    return numkeys - nextOut;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var numkeys = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[numkeys++] = value;
+    Object.assign(someInstance, storage);
   };
 
   someInstance.pop = function() {
+    delete storage.numkeys;
+    if (numkeys > 0) {
+      numkeys--;
+    }
+    return someInstance[numkeys];
   };
 
   someInstance.size = function() {
+    return numkeys;
   };
-
   return someInstance;
 };
